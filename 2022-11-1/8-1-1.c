194a997ae74c06670a9ba342f25eea3f025cd497
MercuryLS <111221528+MercuryLS@users.noreply.github.com>
2022-11-01 11:08:44 +0800

    Add files via upload

---
+++ 2022-11-1/8-1-1.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+    int x,y,*px=&x,*py=&y,*p;
+    char ch;
+    printf("请输入两个整数:");
+    scanf("%d%d",&x,&y);
+    getchar();
+    printf("请指定排序方式(a-升序 d-降序):");
+    scanf("%c",&ch);
+    if(x>y&&ch=='a'||x<y&&ch=='d')
+    {
+        p=px;
+        px=py;
+        py=p;
+    }
+    printf("排序结果:%d\t%d\n",*px,*py);
+    return 0;
+}
---
+++ 2022-11-1/8-1-2.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+void filter(float *,float *,float *, float *,float *);
+
+int main()
+{
+    float x,y,z,max,min;
+    float *px=&x,*py=&y,*pz=&z;
+    printf("请输入三个数:");
+    scanf("%f%f%f",px,py,pz);
+    filter(px,py,pz,&max,&min);
+    printf("最大数:%f\n最小数:%f\n",max,min);
+    return 0;
+}
+
+void filter(float *p1,float *p2,float *p3, float *m,float *n)
+{
+    *m=*p1>*p2? *p1:*p2;
+    *m=*m>*p3? *m:*p3;
+    *n=*p1>*p2? *p2:*p1;
+    *n=*n>*p3? *p3:*n;
+}
